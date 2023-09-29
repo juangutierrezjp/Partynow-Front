3e663a65a5bf4942fbdcda1b12f1a9a2d528ab79
Gonzalo <utngonzalollanos@gmail.com>
2023-09-29 20:50:41 -0300

    Card 2

--- src/Screens/Boliches.jsx
+++ src/Screens/Boliches.jsx
@@ -26,6 +26,14 @@ const Boliches = () => {
           distance="3.5"
           stars={3}
         />
+        <Card
+          imageSource={{
+            uri: "https://viapais.com.ar/resizer/Q0-7t4AZiZdry4y-pRVCpyDh1ZM=/980x640/smart/filters:quality(75):format(webp)/cloudfront-us-east-1.images.arcpublishing.com/grupoclarin/6VZREZL32NA5TB5UBTMYYALDLI.jpg",
+          }} // Corregimos la forma en que se pasa la fuente de la imagen
+          firstName="Boliche Otaku"
+          distance="3.5"
+          stars={3}
+        />
       </View>
     </View>
   );
