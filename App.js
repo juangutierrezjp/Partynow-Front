f866f1e44d0a0ed560a6d5f11c863cfe20ff4443
Nahuel Lautaro Torres Loretto <112157731+Lautaro261@users.noreply.github.com>
2023-12-05 11:51:16 -0300

    configuracion con el servidor

--- App.js
+++ App.js
@@ -7,11 +7,42 @@ import { ApplicationProvider } from "@ui-kitten/components";
 import { light } from "./src/themes";
 import LoginScreen from "./src/Screens/LoginScreen"; // Asegúrate de importar tu pantalla de inicio de sesión
 import RegisterScreen from "./src/Screens/RegisterScreen";
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
+import { setContext } from "apollo-link-context";
+import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev";
+import { MIPORT } from "@env"
+
+if (__DEV__) {  
+  loadDevMessages();
+  loadErrorMessages();
+}
+
+const httpLink = createHttpLink({
+  uri: `${MIPORT}/graphql`
+})
+
+const authLink = setContext(async(_, {headers})=>{  //CONFIGURACION DE TOKEN (AHORA ES NULL)
+ // const token = await AsyncStorage.getItem('token')   // se debe instalar AsyncStorage como sugerencia para almacenar el token
+ const token = null
+  return{
+    headers:{
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ""
+    }
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+})
+
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <>
+    <ApolloProvider client={client}>
       <ApplicationProvider {...eva} theme={light}>
         <NavigationContainer>
           <Stack.Navigator
@@ -25,6 +56,7 @@ function App() {
           </Stack.Navigator>
         </NavigationContainer>
       </ApplicationProvider>
+      </ApolloProvider>
     </>
   );
 }
